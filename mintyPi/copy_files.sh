4a0717b91f8b5474bec3d9091369c9ce8b6fadc2
Unknown <skochw@gmail.com>
2019-07-19 18:36:13 -0500

    Fixing file paths.

--- mintyPi/copy_files.sh
+++ mintyPi/copy_files.sh
@@ -2,31 +2,31 @@
 
 asound_conf=asound.conf
 echo "Copying i2s sound config..."
-cp /boot/mintyPi/$asound_conf  /etc/$asound_conf
+cp /home/pi/mintyPi_setup/mintyPi/$asound_conf  /etc/$asound_conf
 echo "Done!"
 read -p "Press enter to continue"
 
 retroarch_cfg=retroarch.cfg
 echo "Copying retroarch config..."
-cp "/boot/mintyPi/$retroarch_cfg" "/opt/retropie/configs/all/$retroarch_cfg" 
+cp "/home/pi/mintyPi_setup/mintyPi/$retroarch_cfg" "/opt/retropie/configs/all/$retroarch_cfg" 
 echo "Done!"
 read -p "Press enter to continue"
 
 es_cfg=es_input.cfg
 echo "Copying emulationstation input config..."
-cp /boot/mintyPi/$es_cfg /opt/retropie/configs/all/emulationstation/$es_cfg
+cp /home/pi/mintyPi_setup/mintyPi/$es_cfg /opt/retropie/configs/all/emulationstation/$es_cfg
 echo "Done!"
 read -p "Press enter to continue"
 
 es_settings=es_settings.cfg
 echo "Copying emulationstation settings..."
-cp /boot/mintyPi/$es_settings /opt/retropie/configs/all/emulationstation/$es_settings
+cp /home/pi/mintyPi_setup/mintyPi/$es_settings /opt/retropie/configs/all/emulationstation/$es_settings
 echo "Done!"
 read -p "Press enter to continue"
 
 echo "Adding fixes for PIFBA audio"
-sudo cp /boot/mintyPi/.asound.rc /home/pi/
-sudo cp /boot/mintyPi/alsa-base.conf /etc/modprobe.d/
+sudo cp /home/pi/mintyPi_setup/mintyPi/.asound.rc /home/pi/
+sudo cp /home/pi/mintyPi_setup/mintyPi/alsa-base.conf /etc/modprobe.d/
 echo "Done!"
 read -p "Press enter to continue"
 
@@ -34,15 +34,15 @@ echo "Setting up ftp enable/disable scripts..."
 enable_ftp=enable-ftp.sh
 disable_ftp=disable-ftp.sh
 game_list=gamelist.xml
-cp /boot/mintyPi/$enable_ftp /home/pi/RetroPie/retropiemenu/$enable_ftp
-cp /boot/mintyPi/$disable_ftp /home/pi/RetroPie/retropiemenu/$disable_ftp
-cp /boot/mintyPi/$game_list /home/pi/.emulationstation/gamelists/retropie/$game_list
+cp /home/pi/mintyPi_setup/mintyPi/$enable_ftp /home/pi/RetroPie/retropiemenu/$enable_ftp
+cp /home/pi/mintyPi_setup/mintyPi/$disable_ftp /home/pi/RetroPie/retropiemenu/$disable_ftp
+cp /home/pi/mintyPi_setup/mintyPi/$game_list /home/pi/.emulationstation/gamelists/retropie/$game_list
 echo "Done!"
 read -p "Press enter to continue"
 
 echo "Setting up mintyPi startup script..."
 chmod +x /home/pi/mintyPi/minty-startup.sh
-sudo cp /boot/mintyPi/mintyPiStartup.service /etc/systemd/system/mintyPiStartup.service
+sudo cp /home/pi/mintyPi_setup/mintyPi/mintyPiStartup.service /etc/systemd/system/mintyPiStartup.service
 sudo chmod 777 /etc/systemd/system/mintyPiStartup.service
 sudo systemctl enable mintyPiStartup
 echo "Done!"
